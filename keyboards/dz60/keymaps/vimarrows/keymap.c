8ce435bc829c76027d6509b82764cdea36f5d133
Rob Crowell <robccrowell@gmail.com>
2018-07-18 17:29:43 -0700

    shift+backspace is pipe

--- keyboards/dz60/keymaps/vimarrows/keymap.c
+++ keyboards/dz60/keymaps/vimarrows/keymap.c
@@ -7,9 +7,11 @@
 // custom keycodes
 enum custom_keycodes {
   MY_LSHIFT_LBRACE = SAFE_RANGE,
-  MY_RSHIFT_RBRACE
+  MY_RSHIFT_RBRACE,
+  MY_BACKSPACE_PIPE
 };
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  static uint8_t mods_pressed;
   static bool lshift_lbrace_interrupted = false;
   static bool rshift_rbrace_interrupted = false;
   static uint16_t lshift_lbrace_timer = 0;
@@ -41,6 +43,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         unregister_mods(MOD_BIT(KC_RSHIFT));
       }
       return false;
+    case MY_BACKSPACE_PIPE:
+      if (record->event.pressed) {
+	mods_pressed = get_mods();
+	if (mods_pressed & MOD_BIT(KC_LSHIFT)) {
+	  register_code(KC_BSLASH);
+	  unregister_code(KC_BSLASH);
+	} else {
+	  register_code(KC_BSPACE);
+	  unregister_code(KC_BSPACE);
+	}
+      }
   }
   // default:
   lshift_lbrace_interrupted = true;
@@ -116,10 +129,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // standard layer
   [_QWERTY] = KEYMAP(
          KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLASH, TD(TD_ESC_FORCEQUIT),
-         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPACE,
+         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, MY_BACKSPACE_PIPE,
          KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
          KC_LSPO, KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSPC, KC_NO,
-         MO(_FN), KC_LALT, KC_LGUI, KC_SPC, KC_SPC, KC_SPC, KC_RGUI, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT),
+         MO(_FN), KC_LALT, KC_LGUI, KC_SPC, KC_SPC, KC_SPC, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, MO(_FN)),
   // standard fn layer
   [_FN] = KEYMAP(
          RESET, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP,
